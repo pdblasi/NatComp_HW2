43e47e7ac0173a0e07385d2daa9dc3ad0b78026d
SAthow <stephanie.athow@mines.sdsmt.edu>
2015-03-09 15:14:30 -0600

    Comment hill climb code, started on ANN #17

---
+++ BioInspiredHW/code/Neurocomputing_17.py
@@ -0,0 +1,61 @@
+###############################################################################
+# Project: 	 Pg 199 #17 - ANN
+# Author: 	 Stephanie Athow
+# Date: 	 9 March 2015
+# Professor: Dr. McGough
+# Class: 	 Natural Computing
+# Problem Statement:
+#	Apply the MLP network trained with the back-propagation learning algorithm
+#	to solve the character recognition task of section 4.4.2.
+#
+#	Determine a suitable network architecture and then test the resultant
+#	network sensitivity to noise in the test data. Test different noise levels,
+#	from 5% to 50%.
+###############################################################################
+
+# Algorithm from book:
+'''
+def backprop( max_it, min_err, alpha, X, D ):
+	for m from 1 to M:
+		init W^m 	# small random values
+		init b^m	# small random values
+		
+	t = 1
+	while t < max_it and MSE > min_err:
+		vet_permute <- randperm( N )		# permutations of N
+		
+		for j from 1 to N:					# for all input patters
+			# select index i of pattern Xi to be presented
+			i <- vet_permute( j )			# present patterns randomly
+			
+			# forward propagation of the functional signal
+			# eq 4.37
+			
+			for m from O to M-1:
+				# eq 4.36
+				
+			# backprop of sensitivitites
+			# eq 4.65
+			
+			for m from M-1 to 1:
+				# eq 4.60
+			
+			# update weights and biases
+			for m from 1 to M:
+				# eq 4.52
+				# eq 4.53
+				
+			# calculate the error for pattern i
+			# equation in book
+		
+		MSE <- 1 / N.sum( Ei )		# Mean Square Error
+		t += 1
+
+	# end
+'''
+
+# import necessary libraries
+import numpy as np
+import math
+
+# will want to use np.matlib and np.matrix
--- BioInspiredHW/code/nc_hw2_hill_climb.py
+++ BioInspiredHW/code/nc_hw2_hill_climb.py
@@ -1,33 +1,41 @@
-# Hill Climb Programs
-# Author: Stephanie Athow
-# Date: 11 February 2015
+###############################################################################
+# Project: 	 Hill Climb Programs
+# Author: 	 Stephanie Athow
+# Date: 	 11 February 2015
+# Professor: Dr. McGough
+# Class: 	 Natural Computing
+###############################################################################
 
+# Import relevant libraries
 import numpy as np
 import random
 import math
 
 ###############################################################################
 # 						Simple Hill Climb (alg 3.1)
-# initialize x
+# initialize x 
 # loop:
 # 	x'= delx + x
 # 	if x' is better than x, x = x'
 ###############################################################################
 def simple_climb( xtry ):
-	xbest1 = 1.01
+	xbest1 = 1.01	# xbest1, 2, 3 are used to calculate xavg
 	xbest2 = 1.01
 	xbest3 = 1.01
-	xavg = 1.00
+	xavg = 1.00		# calculate avg
+	delx = 0.01		# perterb x
+	
+	ybest = 0.0		# initialize ybest for comparison
 
-	ybest = 0.0
+	i = 0			# loop counter
 
-	delx = 0.01
-
-	i = 0
-
+	# stop at 1000 iterations or no significant improvement is made
 	while (i < 1000) and (xavg > 0.1) :
+		
+		# evaluate x
 		ytry = 2.0 ** (-2.0 * ( ( xtry - 0.1 ) / 0.9 ) ** 2.0) * math.sin( 5.0 * math.pi *xtry ) ** 6.0
 	
+		# if it's better, remember that and make record new 'bests'
 		if ytry > ybest:
 			ybest = ytry
 			xbest3 = xbest2
@@ -40,41 +48,46 @@ def simple_climb( xtry ):
 	
 	return xbest1
 
-
+# initialize x
 xtry = random.random()
 
+# run hill climb
 xbest1 = simple_climb( xtry )
 
-print 'Simple Hill Climb'	
-print 'X = ', xbest1
+print 'Simple Hill Climb', '\n', 'X = ', xbest1
 
 ###############################################################################
 # 						Iterated Hill Climb (alg 3.1)
 # initialize best
 # loop:
 #	initialize x
-#	eval(x)
-# 	x<- simple hill climb
+#	eval(x) via simple hill climb
 # 	if x' is better than best, best = x
 ###############################################################################
-best = 0.0
+
+best = 0.0	# initialize best
+i = 0		# loop counter
+
 besteval = 2.0 ** ( -2.0 * ( ( best - 0.1 ) / 0.9 ) ** 2.0 ) * math.sin( 5.0 * math.pi * best ) ** 6.0
 
-i = 0
-
 while (i < 1000) :
+	# initialize x
 	x = random.random()
+	
+	# run hill climb on x
 	x = simple_climb( x )
-	xeval = 2.0 ** ( -2.0 * ( ( x - 0.1 ) / 0.9 ) ** 2.0 ) * math.sin( 5.0 * math.pi *x ) ** 6.0
+	
+	# evaluate x
+	xeval = 2.0 ** ( -2.0 * ( ( x - 0.1 ) / 0.9 ) ** 2.0 ) * math.sin( 5.0 * math.pi * x ) ** 6.0
+	
+	# if xeval is better than best, store xeval as best
 	if( xeval > besteval ):
 		best = x
 		besteval = xeval
 	
 	i += 1
 
-	
 print '\n', 'Iterated Hill Climb', '\n', 'X = ', best
-
 
 ###############################################################################
 # 						Stochastic Hill Climb (alg 3.3)
@@ -86,20 +99,27 @@ print '\n', 'Iterated Hill Climb', '\n', 'X = ', best
 # 	if random[0,1) < ( 1 / ( 1 + exp[ ( eval x - eval x' ) / T] ) ), 
 #		x = x'
 ###############################################################################
-x = random.random()
+
+x = random.random()		# initialize x
+i = 0					# loop counter
+
+# evaluate x
 xeval = 2.0 ** ( -2.0 * ( ( x - 0.1 ) / 0.9 ) ** 2.0 ) * math.sin( 5.0 * math.pi * x ) ** 6.0
 
-i = 0
-
 while (i < 1000) :
+	# randomly perturb x'
 	xp = x + np.random.normal( 0, 0.2 )
 	
-	while not ( xp <=1 and xp >= 0 ):
-		xp = x + np.random.normal( 0, 0.2 )
+	# check x', must stay between 0 and 1
+	while not ( xp <= 1 and xp >= 0 ):
+		xp = x + np.random.normal( 0, 0.2 )	
 	
+	# evaluate x'
 	xpeval = 2.0 ** ( -2.0 * ( ( xp - 0.1 ) / 0.9 ) ** 2.0 ) * math.sin( 5.0 * math.pi *xp ) ** 6.0
 	
+	# calculate probability of accepting x'
 	p = 1.0 / ( 1.0 + math.exp( (xeval - xpeval) / 0.01 ) )
+	
 	if random.random() < p :
 		x = xp
 		xeval = xpeval
