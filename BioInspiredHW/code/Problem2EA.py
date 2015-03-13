c50275995c6cec753beba044ff2e5d1f40d6a713
Paul Blasi <pdblasi@outlook.com>
2015-03-13 09:08:51 -0600

    Removed old EA file. Got the EA up, but still debugging a weird problem.

--- BioInspiredHW/code/Problem2EA.py
+++ BioInspiredHW/code/Problem2EA.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import numpy as np
 import math
+import random
 
 class EA:
     bit_masks = ( 0b0000000000000001,
@@ -38,20 +39,106 @@ class EA:
 
     def __init__(self, population_size, mutation_rate):
         self.population_size = population_size
+        self.child_population_size = self.population_size // 2
         self.mutation_rate = mutation_rate
-        self.population = np.zeros(dtype=np.int16, shape=(self.population_size))
+        self.population = np.random.random_integers(0, 32767, self.population_size).astype(np.int16)
         self.population_eval = np.zeros(dtype=np.float, shape=(self.population_size))
-        self.__next_population = np.zeros(dtype=np.int16, shape=(self.population_size))
-        self.__next_population_eval = np.zeros(dtype=np.float, shape=(self.population_size))
-        self.__child_population = np.zeros(dtype=np.int16, shape=(math.ceil(self.population_size / 2)))
-        self.__child_population_eval = np.zeros(dtype=np.int16, shape=(math.ceil(self.population_size / 2)))
+        self.__next_population = np.zeros(dtype=np.int16, shape=(self.population_size  + self.child_population_size))
+        self.__next_population_eval = np.zeros(dtype=np.float, shape=(self.population_size + self.child_population_size))
+        self.__child_population = np.zeros(dtype=np.int16, shape=(self.child_population_size))
+        self.__child_population_eval = np.zeros(dtype=np.float, shape=(self.child_population_size))
+        self.__child_pop_indices = range(self.child_population_size)
+        self.__pop_indices = range(self.population_size)
+
+        self.evaluate(self.population, self.population_eval);
 
     def print_best(self):
-        return
+        index = 0
+        max = 0
+        for i in self.__pop_indices:
+            if self.population_eval[i] > max:
+                max = self.population_eval[i]
+                index = i
+        print(self.binary_format.format(self.population[index]), "Value:", max)
 
-    def eval(self):
-        return
+    def evaluate(self, pop, eval):
+        pop.size
+        for i in range(pop.size):
+            x = np.float32(pop[i]) / 32767
+            eval[i] = (2 ** (-2 * (((x - 0.1)/.9) * ((x - 0.1)/.9))) * math.sin(5 * math.pi * x) ** 6)
+
+    def select_indices(self, count, eval):
+        unused = range(eval.size)
+        indices = []
+        i = 0
+        sum = np.sum(eval)
+
+        while i < count:
+            i = i+1
+            rand = np.random.uniform(0, sum)
+            index = 0
+            curr = eval[unused[index]]
+
+            try:
+                while (rand - curr) < 1e-10:
+                    index = index + 1
+                    curr = curr + eval[unused[index]]
+            except:
+                print("Index:", index, "Unused:", "len({0})".format(len(unused)), unused)
+                print("Random:", rand, "Curr:", curr, "Last Added:", eval[unused[index - 1]])
+
+            sum = sum - eval[unused[index]]
+            indices.append(unused.pop(index))
+
+        return indices
+
+    def create_children(self, indices):
+        for i in self.__child_pop_indices:
+            child = self.combine(self.population[indices[2 * i]], self.population[indices[2 * i + 1]])
+            if np.random.uniform() < self.mutation_rate:
+                child = self.mutate(child)
+
+            self.__child_population[i] = child
+
+        self.evaluate(self.__child_population, self.__child_population_eval)
+
+        for i in self.__child_pop_indices:
+            self.__next_population[self.population_size + i] = self.__child_population[i]
+            self.__next_population_eval[self.population_size + i] = self.__child_population_eval[i]
+
+    def combine(self, mom, dad):
+        mask = self.reproduction_masks[random.randint(0, len(self.reproduction_masks) - 1)]
+        return (mom & mask) | (dad & ~mask)
+
+    def mutate(self, child):
+        mask = self.bit_masks[random.randint(0, len(self.bit_masks) - 1)]
+        return child ^ mask
+
+    def run(self, iterations, printEvery):
+        i = 0
+        while i < iterations:
+            i = i + 1
+
+            for j in self.__pop_indices:
+                self.__next_population[j] = self.population[j]
+                self.__next_population_eval[j] = self.population_eval[j]
+
+            self.create_children(self.select_indices(self.population_size, self.population_eval))
+
+            indices = self.select_indices(self.population_size, self.__next_population_eval);
+
+            for j in self.__pop_indices:
+                self.population[j] = self.__next_population[indices[j]]
+                self.population[j] = self.__next_population_eval[indices[j]]
+
+            if i != iterations and i % printEvery == 0:
+                self.print_best();
+
+        self.print_best();
+
+
 
 if __name__ == '__main__':
-    test = EA(1000, .01)
+    test = EA(200, .01)
+    test.run(1000, 10)
     print("hello world.")
